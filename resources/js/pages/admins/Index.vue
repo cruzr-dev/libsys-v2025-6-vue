ef2d0b08c2b910027a25802f9eefad3536dec918
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-19 11:53:48 +0800

    pwede na ni

--- resources/js/pages/admins/Index.vue
+++ resources/js/pages/admins/Index.vue
@@ -285,19 +285,24 @@ function buildFilters(filtersArr: ColumnFiltersState) {
                                     :key="column.id"
                                     :checked="column.getIsVisible()"
                                     @update:checked="(value) => column.toggleVisibility(!!value)"
-                                    class="relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none hover:bg-gray-100 data-[state=checked]:pl-8"
+                                    class="relative flex cursor-pointer select-none items-center rounded-sm pl-8 pr-2 py-1.5 text-sm outline-none hover:bg-gray-100"
                                 >
-                                  <span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-                                    <svg
-                                        v-if="column.getIsVisible()"
-                                        class="h-4 w-4"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                    >
-                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-                                    </svg>
-                                  </span>
+      <span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+        <svg
+            v-if="column.getIsVisible()"
+            class="h-4 w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+        >
+          <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 13l4 4L19 7"
+          />
+        </svg>
+      </span>
                                     {{ column.id }}
                                 </DropdownMenuCheckboxItem>
                             </DropdownMenuContent>
