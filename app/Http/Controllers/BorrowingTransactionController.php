24a232186ab5670a3c4ef98efea8e71fd6aa0d06
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-22 05:09:18 +0800

    feat: /t.n. number/ borrow index, return, success, refresh nalang kulang

--- app/Http/Controllers/BorrowingTransactionController.php
+++ app/Http/Controllers/BorrowingTransactionController.php
@@ -251,7 +251,6 @@ class BorrowingTransactionController extends Controller
             }
 
             // Generate unique transaction number
-            $transactionNumber = 'BRW-I' . date('ymdHis') . '-' . str_pad(random_int(1, 99999), 4, '0', STR_PAD_LEFT);
             $record_id = Record::where('accession_number', $request->accession_number)->first()->id;
 
             if (!$record_id) {
