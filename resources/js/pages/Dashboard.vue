4a11fcbb6d3d81b0481b37fdb145fa89411911b1
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-27 12:34:16 +0800

    feat: dashboard, placeholder

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -22,29 +22,7 @@ defineProps({
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-            <div class="flex gap-2">
-                <Link :href="route('borrowings.create')">
-                    <Button variant="secondary">Borrow Book</Button>
-                </Link>
-            </div>
-            <div class="grid grid-cols-4 gap-2">
-                <Card class="p-4">
-                    No. of users:
-                    {{ userCount }}
-                </Card>
-                <Card class="p-4">
-                    No. of records:
-                    {{ userCount }}
-                </Card>
-                <Card class="p-4">
-                    No. of users:
-                    {{ userCount }}
-                </Card>
-                <Card class="p-4">
-                    No. of users:
-                    {{ userCount }}
-                </Card>
-            </div>
+            dashboard here
         </div>
     </AppLayout>
 </template>
