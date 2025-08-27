d07acf13acf1a7a973ba1f32be396d8feada1cf4
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-27 13:45:43 +0800

    feat: users import, ctet seeder

--- database/seeders/AcademicSeeder.php
+++ database/seeders/AcademicSeeder.php
@@ -15,28 +15,33 @@ class AcademicSeeder extends Seeder
     public function run(): void
     {
         // Seed Colleges
-        $coe = College::create([
-            'code' => 'COE',
-            'name' => 'College of Engineering',
+        $ctet = College::create([
+            'code' => 'CTET',
+            'name' => 'College of Teacher Education and Technology',
         ]);
 
-        // Seed Programs
-        DB::table('programs')->insert([
-            ['code' => 'BSCS', 'name' => 'Bachelor of Science in Computer Science'],
-            ['code' => 'BSEd', 'name' => 'Bachelor of Secondary Education'],
-            ['code' => 'BSBA', 'name' => 'Bachelor of Science in Business Administration'],
-            ['code' => 'BSIT', 'name' => 'Bachelor of Science in Information Technology'],
-            ['code' => 'BSN',  'name' => 'Bachelor of Science in Nursing'],
-            ['code' => 'NA', 'name' => 'Not Applicable'],
+        $ctet->courses()->createMany([
+            [
+                'code' => 'BSED',
+                'name' => 'Bachelor of Secondary Education',
+            ],
+            [
+                'code' => 'BEED',
+                'name' => 'Bachelor of Elementary Education',
+            ],
+            [
+                'code' => 'BECED',
+                'name' => 'Bachelor of Early Childhood Education',
+            ],
+            [
+                'code' => 'BSNED',
+                'name' => 'Bachelor of Special Needs Education',
+            ],
+            [
+                'code' => 'BSIT',
+                'name' => 'Bachelor Science in Information Technology',
+            ],
         ]);
 
-        // Seed Majors
-        DB::table('majors')->insert([
-            ['name' => 'Filipino'],
-            ['name' => 'Mathematics'],
-            ['name' => 'Information Security'],
-            ['name' => 'Marketing'],
-            ['name' => 'Biology'],
-        ]);
     }
 }
