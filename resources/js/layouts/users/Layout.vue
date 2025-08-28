fb3baff0936f2f1d7414d376844b98b2d143a6ee
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-28 08:54:54 +0800

    feat: singular lang

--- resources/js/layouts/users/Layout.vue
+++ resources/js/layouts/users/Layout.vue
@@ -10,7 +10,7 @@ const sidebarNavItems: NavItem[] = [
         href: '/users/all',
     },
     {
-        title: 'Students',
+        title: 'Student',
         href: '/users/students',
     },
     {
