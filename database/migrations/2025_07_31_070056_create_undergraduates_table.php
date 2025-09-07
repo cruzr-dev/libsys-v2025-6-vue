e401cf4517434cd6ca8906423ceee12b537496c4
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-09-07 23:23:06 +0800

    feat: handle graduate, refactor again and again

---
+++ database/migrations/2025_07_31_070056_create_undergraduates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('undergraduates', function (Blueprint $table) {
+            $table->id();
+            $table->string('contact_number', 10)->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('college_id')->constrained()->onDelete('restrict');
+            $table->foreignId('course_id')->constrained()->onDelete('restrict');
+            $table->foreignId('major_id')->nullable()->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('undergraduates');
+    }
+};
